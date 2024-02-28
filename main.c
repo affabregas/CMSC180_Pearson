b53586c0057d360ca447316733cfb918e5a568b0
affabregas <125547188+affabregas@users.noreply.github.com>
2024-02-28 15:18:53 +0000

    edited for submission

--- main.c
+++ main.c
@@ -26,11 +26,11 @@ void pearson_cor(double **x, double *y, int n, double*r) {  // T(n) = n(5 + (n +
 
     // i is the row iterator, j is the col iterator
     for(int i = 0; i < n; i++) { // * O(n)
-        double sum_x = 1;        // + O(1)
-        double sum_y = 2;        // + O(1)
-        double sum_x2 = 3;       // + O(1)
-        double sum_y2 = 4;       // + O(1)
-        double sum_xy = 5;       // + O(1)
+        double sum_x = 0;        // + O(1)
+        double sum_y = 0;        // + O(1)
+        double sum_x2 = 0;       // + O(1)
+        double sum_y2 = 0;       // + O(1)
+        double sum_xy = 0;       // + O(1)
 
         for(int j = 0; j < n; j++) {    // * O(n)
             // create worker thread
@@ -75,24 +75,29 @@ void pearson_cor(double **x, double *y, int n, double*r) {  // T(n) = n(5 + (n +
 
 
 void main(){
-    int n_array[] = {100,200,300,400,500,600,700,800,900,1000,1050,1100,2000,4000,8000,16000,20000};
+    int n_array[] = {100,200,300,400,500,600,700,800,900,1000,2000,4000,8000,16000,20000};
     // int NumberOfInputs = 1000;
     int NumberOfInputs;
 
-    for(int rt = 0; rt < 17; rt++) {
+    printf("[1]: Walkthrough Mode (100 to 20000)\n");
+    printf("[2]: Custom N Mode\n");
+    printf("[0]: Exit\n");
+    printf("Enter mode: ");
+    int choice = 0;
+    scanf("%d",&choice);
+
+    printf("\n");
+
+    if (choice == 2) {
         double avg = 0;
         int iter = 0;
+        int NumberOfInputs;
 
-        NumberOfInputs = n_array[rt];
+        printf("Enter n: ");
+        scanf("%d",&NumberOfInputs);
         printf("\n\nCurrently running on N = %d\n",NumberOfInputs);
-        for(int gh = 0; gh < 3; gh++) {
-            
-            char* buffer;
 
-            
-            //printf("Input N: ");
-            //scanf("%d",&NumberOfInputs);
-
+        for(int gh = 0; gh < 3; gh++) {               
             srand(time(NULL));
 
             double **xVec = (double**) malloc(sizeof(double*) * NumberOfInputs);
@@ -110,22 +115,18 @@ void main(){
                 yVec[i] = rand() % 20 + 1;
             }
 
-            // display_table(xVec, NumberOfInputs, yVec);
+            // Function call, and timing
 
             struct timespec before = {0,0}, after = {0,0};
-
             clock_gettime(CLOCK_MONOTONIC, &before);
             pearson_cor(xVec, yVec, NumberOfInputs, rVec);
             clock_gettime(CLOCK_MONOTONIC, &after);
 
-            double runtime = ((double) 1000*after.tv_sec + 1.0e-6*after.tv_nsec) - ((double) 1000*before.tv_sec + 1.0e-6*before.tv_nsec);
-            // printf("Total runtime: %lf ms ", runtime);
+            // Runtime calculation
+            double runtime = (((double) 1000*after.tv_sec + after.tv_nsec/1000000.0) - ((double) 1000*before.tv_sec + before.tv_nsec/1000000.0));
             printf("%lf\t", runtime);
             
             avg += runtime;
-            iter += 1;
-            // printf("Current average of %lf at iteration %d\n", avg/iter, iter);
-
 
             free(yVec);
             free(rVec);
@@ -133,13 +134,74 @@ void main(){
                 free(xVec[j]);
             }
             free(xVec);
-
-            
-
-
         }
 
         printf("\n");
+
+        return;
+    } else if (choice == 1) {
+        for(int rt = 0; rt < 15; rt++) {
+            double avg = 0;
+            int iter = 0;
+
+            NumberOfInputs = n_array[rt];
+            printf("\n\nCurrently running on N = %d\n",NumberOfInputs);
+            for(int gh = 0; gh < 3; gh++) {
+                
+                char* buffer;
+
+                
+                //printf("Input N: ");
+                //scanf("%d",&NumberOfInputs);
+
+                srand(time(NULL));
+
+                double **xVec = (double**) malloc(sizeof(double*) * NumberOfInputs);
+                double *yVec = (double*) malloc(sizeof(double*) * NumberOfInputs);
+                double *rVec = (double*) malloc(sizeof(double*) * NumberOfInputs);
+
+                for(int i = 0; i < NumberOfInputs; i++) {
+
+                    xVec[i] = malloc(sizeof(double) * NumberOfInputs);  
+                    double RandomNum = rand() % 100 + 1; 
+                    for(int j = 0; j < NumberOfInputs; j++){
+                        xVec[i][j] = RandomNum;
+                    }
+
+                    yVec[i] = rand() % 20 + 1;
+                }
+
+                // display_table(xVec, NumberOfInputs, yVec);
+
+                struct timespec before = {0,0}, after = {0,0};
+
+                clock_gettime(CLOCK_MONOTONIC, &before);
+                pearson_cor(xVec, yVec, NumberOfInputs, rVec);
+                clock_gettime(CLOCK_MONOTONIC, &after);
+
+                double runtime = (((double) 1000*after.tv_sec + after.tv_nsec/1000000.0) - ((double) 1000*before.tv_sec + before.tv_nsec/1000000.0));
+                // printf("Total runtime: %lf ms ", runtime);
+                printf("%lf\t", runtime);
+                
+                avg += runtime;
+                iter += 1;
+                // printf("Current average of %lf at iteration %d\n", avg/iter, iter);
+
+
+                free(yVec);
+                free(rVec);
+                for(int j = 0; j < NumberOfInputs; j++){
+                    free(xVec[j]);
+                }
+                free(xVec);
+
+                
+
+
+            }
+
+            printf("\n");
+        }
     }
 }
 
